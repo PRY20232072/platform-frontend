cb438263f70e038f3beb0f098329aae6152d9c51
MechaXAI <adrianxalzamora@gmail.com>
2023-11-12 20:49:17 -0500

    feat: added ui components

---
+++ components/ui/auto-complete.tsx
@@ -0,0 +1,24 @@
+'use client'
+import React from 'react';
+import { Autocomplete, AutocompleteItem } from '@nextui-org/react'; 
+
+interface CustomAutocompleteProps {
+  label: string;
+  labelPlacement: any;
+  placeholder: string;
+  data: { value: string; label: string }[];
+}
+
+const CustomAutocomplete: React.FC<CustomAutocompleteProps> = ({ label, labelPlacement, placeholder, data }) => (
+  <div className="mb-4">
+    <Autocomplete label={label} labelPlacement={labelPlacement} placeholder={placeholder}>
+      {data.map((item) => (
+        <AutocompleteItem key={item.value} value={item.value}>
+          {item.label}
+        </AutocompleteItem>
+      ))}
+    </Autocomplete>
+  </div>
+);
+
+export { CustomAutocomplete};
