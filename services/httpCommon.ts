38971c23faec42d07d4d8a57b6a0749e3b4c9718
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-11-23 02:37:44 -0500

    [develop]: add connecto to deployed blockchain server

--- services/httpCommon.ts
+++ services/httpCommon.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:4000",
+    // baseURL: "http://localhost:4000",
+    baseURL: "https://sawtooth-platform-rest-api.azurewebsites.net/",
     headers: {
         "Content-type": "application/json",
         'Access-Control-Allow-Origin' : '*',
