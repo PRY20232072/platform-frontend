3e9c1c6f700a2121e1e1d0a076af4764dd09acb6
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-11-23 01:51:02 -0500

    [patient-allergies]: remove unused imports

--- app/patient/[patientId]/(routes)/notifications/page.tsx
+++ app/patient/[patientId]/(routes)/notifications/page.tsx
@@ -1,23 +1,8 @@
 "use client";
 
-import { Button, Input } from '@nextui-org/react';
-
-import {
-  accessRequestTableColumns,
-  practitionersAccessRequests,
-  eventsTableColumns,
-  events,
-} from '@/data/data';
-import { getServerSession } from 'next-auth/next';
-import { CustomAutocomplete } from '@/components/ui/auto-complete';
-import { User2, Pencil } from 'lucide-react';
 import { AccessRequestTable } from './components/access-request-table';
-import { EventsTable } from './components/events-table';
-import { useSession } from 'next-auth/react';
 
 export default function NotificationPage() {
-
-
   return (
     <div className="flex flex-col  items-center gap-5 px-4 py-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-32">
       <div className="justify-between items-center  border-b border-gray-200 flex w-full max-w-[1100px] gap-5 mt-1 max-md:max-w-full max-md:flex-wrap">
