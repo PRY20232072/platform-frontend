f64871cba0791e1e5fa574e91005f886fd6a4b44
Sebastian Bran <sebastianbranzapata@gmail.com>
2023-11-28 15:06:54 -0500

    [feature/sprint-4]: add card skeletons

--- components/practitioner/practitioner-home.tsx
+++ components/practitioner/practitioner-home.tsx
@@ -7,6 +7,8 @@ import patientService from '@/services/patientService';
 import { User, FileText } from 'lucide-react';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
+import CustomSuspense from '../custom-suspense';
+import CardSkeleton from '../ui/skeletons/card-skeleton';
 
 export default function PractitionerHome() {
   const [allergyList, setAllergyList] = useState<any>([]);
@@ -57,32 +59,36 @@ export default function PractitionerHome() {
   return (
     <>
       <div className="flex flex-wrap items-center justify-center gap-5 px-4 sm:px-6 md:px-8 lg:px-20 xl:px-24 2xl:px-32 py-0 relative self-stretch w-full mt-6 mb-6 flex-[0_0_auto]">
-        <Card
-          link={`practitioner/${session?.user?.id}/patients`}
-          card_title="Patients"
-          icon={
-            <User
-              color="white"
-              className="w-5 h-[18px] left-[2px] top-[3px] absolute"
-            />
-          }
-          heading_one="Name"
-          heading_two="ID"
-          cardData={patientsList}
-        />
-        <Card
-          link={`practitioner/${session?.user?.id}/health-records`}
-          card_title="Health Records"
-          icon={
-            <FileText
-              color="white"
-              className="w-4 h-5 left-[4px] top-[2px] absolute"
-            />
-          }
-          heading_one="Detail"
-          heading_two="Date"
-          cardData={allergyList}
-        />
+        <CustomSuspense isLoading={patientsResponse.isLoading} fallback={<CardSkeleton />}>
+          <Card
+            link={`practitioner/${session?.user?.id}/patients`}
+            card_title="Patients"
+            icon={
+              <User
+                color="white"
+                className="w-5 h-[18px] left-[2px] top-[3px] absolute"
+              />
+            }
+            heading_one="Name"
+            heading_two="ID"
+            cardData={patientsList}
+          />
+        </CustomSuspense>
+        <CustomSuspense isLoading={allergyRecordsResponse.isLoading} fallback={<CardSkeleton />}>
+          <Card
+            link={`practitioner/${session?.user?.id}/health-records`}
+            card_title="Health Records"
+            icon={
+              <FileText
+                color="white"
+                className="w-4 h-5 left-[4px] top-[2px] absolute"
+              />
+            }
+            heading_one="Detail"
+            heading_two="Date"
+            cardData={allergyList}
+          />
+        </CustomSuspense>
       </div>
     </>
   );
