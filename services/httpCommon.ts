27e05aa28dce3d8467c0c023d84aea3d562430a7
Sebastian Bran <66882262+SebastianBran@users.noreply.github.com>
2024-03-06 00:07:49 -0500

    [feature/sprint-5]: pass access token as header in requests

--- services/httpCommon.ts
+++ services/httpCommon.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
+import { getSession } from "next-auth/react";
 
-export default axios.create({
-    // baseURL: "http://localhost:4000/",
-    baseURL: "https://sawtooth-platform-rest-api.azurewebsites.net/",
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:4002/",
+    // baseURL: "https://sawtooth-platform-rest-api.azurewebsites.net/",
     headers: {
         "Content-type": "application/json",
         'Access-Control-Allow-Origin' : '*',
         'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
     }
 });
+
+axiosInstance.interceptors.request.use(async (config) => {
+    const session = await getSession();
+    if (session?.accessToken) {
+        config.headers.Authorization = `Bearer ${session?.accessToken}`;
+    }
+    return config;
+});
+
+export default axiosInstance;
