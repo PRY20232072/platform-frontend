d6e5c38106354ab75ee3ebc475988a75f937ec3c
MechaXAI <adrianxalzamora@gmail.com>
2024-03-30 22:07:18 -0500

    [core]: defined env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config()
+
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  env: {
+    USER_FLOW: process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW,
+    TENANT_ID: process.env.AZURE_AD_B2C_TENANT_ID,
+    TENANT_NAME: process.env.AZURE_AD_B2C_TENANT_NAME,
+    AUTH_URL: process.env.NEXTAUTH_URL,
+    BASE_URL: process.env.BASE_URL,
+  }
 }
 
 module.exports = nextConfig
