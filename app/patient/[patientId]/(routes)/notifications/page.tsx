47e8b09f8247265cb754a6fce5df2e2ff104c5e4
Sebastian Bran <66882262+SebastianBran@users.noreply.github.com>
2024-05-05 00:15:28 -0500

    [feature/sprint-6]: increase the width of the notification section tables

--- app/patient/[patientId]/(routes)/notifications/page.tsx
+++ app/patient/[patientId]/(routes)/notifications/page.tsx
@@ -6,7 +6,7 @@ import PlatformEventsTable from "./components/platform-events-table";
 
 export default function NotificationPage() {
   return (
-    <div className="flex flex-col  items-center gap-5 px-4 py-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-32">
+    <div className="flex flex-col  items-center gap-5 px-4 py-4 sm:px-6 md:px-8 lg:px-16 xl:px-20">
       <div className="justify-between items-center  border-b border-gray-200 flex w-full max-w-[1100px] gap-5 mt-1 max-md:max-w-full max-md:flex-wrap">
         <div className="flex grow basis-[0%] flex-col items-stretch my-auto max-md:max-w-full">
           <div className="mb-4 text-4xl font-bold leading-10 max-md:max-w-full">
@@ -15,7 +15,7 @@ export default function NotificationPage() {
         </div>
       </div>
 
-      <div className="w-full max-w-4xl mx-auto">
+      <div className="w-full mx-auto">
         <div className="mb-8">
           <AccessRequestTable />
         </div>
