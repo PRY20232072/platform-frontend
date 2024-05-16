2313c48ef3952653e1016bb1d8fc4545e9197bae
Sebastian Bran <66882262+SebastianBran@users.noreply.github.com>
2024-05-16 03:35:28 -0500

    [feature/v2]: add documents section

---
+++ app/patient/[patientId]/(routes)/documents/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import CustomSuspense from "@/components/custom-suspense";
+import DocumentsTable from "@/components/documents/documents-table";
+import Loading from "@/components/loading";
+import { useApi } from "@/hooks/useApi";
+import patientService from "@/services/patientService";
+import { RegisterFile } from "@/types/registerFile";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const DocumentsPage = () => {
+  const [files, setFiles] = useState<RegisterFile[]>([]);
+  const { response: getPatientByIdResponse, fetchData: getPatientById } =
+    useApi();
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (session?.user?.id) {
+        await getPatientById(
+          patientService.getPatientById(session?.user?.id as string)
+        );
+      }
+    };
+
+    fetchData();
+  }, [session?.user?.id]);
+
+  useEffect(() => {
+    if (getPatientByIdResponse.isSuccess) {
+      setFiles(getPatientByIdResponse.data.files || []);
+    }
+  }, [getPatientByIdResponse]);
+
+  return (
+    <div>
+      <div className="w-full max-w-[920px] mx-auto mt-6">
+        <h1 className="text-4xl font-bold leading-10 mb-2">Documentos</h1>
+        <CustomSuspense isLoading={getPatientByIdResponse.isLoading} fallback={<Loading />} >
+          <DocumentsTable items={files} />
+        </CustomSuspense>
+      </div>
+    </div>
+  );
+};
+
+export default DocumentsPage;
