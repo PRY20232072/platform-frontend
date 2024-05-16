dfbc81034c6dadfe5efeb65783fe5be30f24a1dc
Sebastian Bran <66882262+SebastianBran@users.noreply.github.com>
2024-05-16 17:53:50 -0500

    [feature/v2]: add return button

--- app/practitioner/[practitionerId]/(routes)/patients/[patientId]/allergy-intolerance/[allergyIntoleranceId]/page.tsx
+++ app/practitioner/[practitionerId]/(routes)/patients/[patientId]/allergy-intolerance/[allergyIntoleranceId]/page.tsx
@@ -3,10 +3,12 @@
 import React from "react";
 import { PatientAllergyClient } from "./components/patient-allergy-client";
 import PatientBasicInfo from "./components/patient-basic-info";
+import ReturnButton from "@/components/ui/return-button";
 
 const SelectedAllergyPatientDetails = () => {
   return (
-    <div className="flex flex-col gap-5 px-4 py-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-32 items-stretch">
+    <div className="flex flex-col gap-5 px-4 py-4 sm:px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-32 items-stretch relative">
+      <ReturnButton />
       <PatientBasicInfo />
       <PatientAllergyClient />
     </div>
---
+++ components/ui/return-button.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@nextui-org/react";
+
+const ReturnButton = () => {
+  return (
+    <Button
+      className="font-medium w-[77px] absolute right-0 mr-4 sm:mr-6 md:mr-8 lg:mr-16 xl:mr-20 2xl:mr-32 mt-2"
+      onClick={() => {
+        window.history.back();
+      }}
+    >
+      Regresar
+    </Button>
+  );
+};
+
+export default ReturnButton;
