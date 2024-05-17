1dc26aa18e79ae40d5913d55aac1de187a4aba27
Sebastian Bran <66882262+SebastianBran@users.noreply.github.com>
2024-05-16 20:51:47 -0500

    [feature/v2]: update return button

--- components/ui/return-button.tsx
+++ components/ui/return-button.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@nextui-org/react";
 
-const ReturnButton = () => {
+const ReturnButton = ({
+  className,
+}: { className?: string}) => {
   return (
     <Button
-      className="font-medium w-[77px] absolute right-0 mr-4 sm:mr-6 md:mr-8 lg:mr-16 xl:mr-20 2xl:mr-32 mt-2"
+      className={"font-medium w-[77px] absolute right-0 mr-4 sm:mr-6 md:mr-8 lg:mr-16 xl:mr-20 2xl:mr-32 mt-2 " + (className || '')}
       onClick={() => {
         window.history.back();
       }}
